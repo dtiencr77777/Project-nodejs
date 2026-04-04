f86060107663a03e55799bb06057080bf06bdca7
Do Tien <tiendt4180@ut.edu.vn>
2026-04-04 21:57:32 +0700

    6_10_1 truyền thêm giá trị cho validate dữ liệu

--- validates/product.validate.js
+++ validates/product.validate.js
@@ -1,7 +1,12 @@
 module.exports.createProducts = (req, res, next) => {
   // validate dữ liệu
-  if (!req.body.title) {
-    req.flash("error", "Vui lòng nhập tên sản phẩm");
+  if (
+    !req.body.title ||
+    !req.body.price ||
+    !req.body.discountPercentage ||
+    !req.body.stock
+  ) {
+    req.flash("error", "Vui lòng điền đầy đủ thông tin sản phẩm");
     res.redirect(req.get("Referrer") || "/");
     return;
   }
