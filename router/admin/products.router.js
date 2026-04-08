bcf67561dbe81ede8092fa2f48caaf1d5e03d032
Do Tien <tiendt4180@ut.edu.vn>
2026-04-08 22:53:42 +0700

    7.5_2 fix upload cloudinary không hiển thị

--- router/admin/products.router.js
+++ router/admin/products.router.js
@@ -37,32 +37,37 @@ router.post(
   "/create",
   upload.single("thumbnail"),
   function (req, res, next) {
-    let streamUpload = (req) => {
-      return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream((error, result) => {
-          if (result) {
-            resolve(result);
-          } else {
-            reject(error);
-          }
+    if (req.file) {
+      let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+          let stream = cloudinary.uploader.upload_stream((error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          });
+
+          streamifier.createReadStream(req.file.buffer).pipe(stream);
         });
+      };
 
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-      });
-    };
+      async function upload(req) {
+        let result = await streamUpload(req);
+        // console.log(result);
 
-    async function upload(req) {
-      let result = await streamUpload(req);
-      // console.log(result);
+        console.log(result.secure_url);
+        // req.body.thumbnail = result.secure_url;
+        req.body[req.file.fieldname] = result.secure_url;
+        next();
+      }
 
-      console.log(result.secure_url);
-      // req.body.thumbnail = result.secure_url;
-      req.body[req.file.fieldname] = result.secure_url;
+      upload(req);
+    } else {
+      next();
     }
-
-    upload(req);
-    next();
   },
+
   validate.createProducts,
   controler.createPost,
 );
