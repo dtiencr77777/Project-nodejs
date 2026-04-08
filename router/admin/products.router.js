aa0aa07031b65da9dc22c7c6030b999a23a47226
Do Tien <tiendt4180@ut.edu.vn>
2026-04-08 21:40:30 +0700

    7.5_1 function cloudinary

--- router/admin/products.router.js
+++ router/admin/products.router.js
@@ -36,6 +36,28 @@ router.get("/create", controler.create);
 router.post(
   "/create",
   upload.single("thumbnail"),
+  function (req, res, next) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+    }
+
+    upload(req);
+  },
   validate.createProducts,
   controler.createPost,
 );
