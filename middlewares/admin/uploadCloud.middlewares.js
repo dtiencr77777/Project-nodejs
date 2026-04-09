a7ed71f11726dadfbdf4e2f9f20e7efaafb4b3bf
Do Tien <tiendt4180@ut.edu.vn>
2026-04-09 20:25:51 +0700

    7.5_4 thêm biến env của cloudinary

--- middlewares/admin/uploadCloud.middlewares.js
+++ middlewares/admin/uploadCloud.middlewares.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 cloudinary.config({
-  cloud_name: "dcoujkven",
-  api_key: "558121248779264",
-  api_secret: "YnRpvuwz74HVJesDSjkG41CaifE", // Click 'View API Keys' above to copy your API secret
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
 module.exports.uploadCloud = (req, res, next) => {
