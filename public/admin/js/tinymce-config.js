e56b7f8394c61af8da0b71f08dcf457d4d262436
Do Tien <tiendt4180@ut.edu.vn>
2026-04-12 20:43:12 +0700

    8.2_1 các tính năng của tinymce để insert imgaes,..... cho description

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,4 +1,6 @@
 tinymce.init({
   selector: "textarea",
   license_key: "gpl",
+  plugins:
+    "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount",
 });
