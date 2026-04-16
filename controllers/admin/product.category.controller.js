0674d72d713c45dedb8e06623002ff538031c413
Do Tien <tiendt4180@ut.edu.vn>
2026-04-16 22:32:45 +0700

    9.3 phân cấp trong products-category

--- controllers/admin/product.category.controller.js
+++ controllers/admin/product.category.controller.js
@@ -6,10 +6,28 @@ module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
-  const productCategory = await ProductCategory.find(find);
+
+  //  hàm tạo cây, đệ quy
+  function createTree(arr, parentId = "") {
+    const tree = [];
+    arr.forEach((item) => {
+      if (item.parent_id == parentId) {
+        const newItem = item;
+        const children = createTree(arr, item.id);
+        if (children.length > 0) {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
+
+  const records = await ProductCategory.find(find);
+  const newRecords = createTree(records);
   res.render("admin/pages/product-category/index.pug", {
     pageTitle: "Danh mục sản phẩm",
-    records: productCategory,
+    records: newRecords,
   });
 };
 
