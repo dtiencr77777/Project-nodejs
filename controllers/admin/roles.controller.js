77a502204ded1ed73abe9c1e39e83725696b1475
Do Tien <tiendt4180@ut.edu.vn>
2026-04-21 21:46:26 +0700

    10.7_2 interface  create roles

--- controllers/admin/roles.controller.js
+++ controllers/admin/roles.controller.js
@@ -1,6 +1,18 @@
+const Role = require("../../models/roles.models");
+
 //  GET /admin/roles
-module.exports.rolesIndex = (req, res) => {
+module.exports.rolesIndex = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+  const records = await Role.find(find);
   res.render("admin/pages/roles/index.pug", {
     pageTitle: "Roels",
+  });
+};
+
+module.exports.create = (req, res) => {
+  res.render("admin/pages/roles/create.pug", {
+    pageTitle: "Tạo nhóm quyền",
   });
 };
--- router/admin/roles.router.js
+++ router/admin/roles.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controler = require("../../controllers/admin/roles.controller");
 
 router.get("/", controler.rolesIndex);
+router.get("/create", controler.create);
 
 module.exports = router;
