33978f25781e7cb99c27c05163fe0550a90f287e
Do Tien <tiendt4180@ut.edu.vn>
2026-05-04 23:14:49 +0700

    12.2_ 2 thêm uploand img and valdidate dữ liệu

--- router/admin/account.router.js
+++ router/admin/account.router.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+//  Hiển thị ảnh sản phẩm
+const multer = require("multer");
+// const upload = multer({ dest: "./public/uploads/" });
+
+//  cloudinary
+// const storageMulter = require("../../helpers/storageMulter");
+// const upload = multer({ storage: storageMulter() });
+const upload = multer();
+const uploadCloudRouter = require("../../middlewares/admin/uploadCloud.middlewares");
+//end  cloudinary
+// validate dữ liệu
+const validate = require("./../../validates/admin/account.validate");
+// controller
 const controler = require("../../controllers/admin/accounts.controller");
 
 router.get("/", controler.accountsDashboard);
 router.get("/create", controler.create);
-router.post("/create", controler.createPost);
+router.post(
+  "/create",
+  upload.single("avatar"),
+  uploadCloudRouter.uploadCloud,
+  validate.createAccounts,
+  controler.createPost,
+);
 
 module.exports = router;
