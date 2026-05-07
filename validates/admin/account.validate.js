affa5397d1707e21abfea5a0951b789d155147e6
Do Tien <tiendt4180@ut.edu.vn>
2026-05-07 15:15:32 +0700

    12.7_1 validate Editaccounts

--- validates/admin/account.validate.js
+++ validates/admin/account.validate.js
@@ -8,3 +8,14 @@ module.exports.createAccounts = (req, res, next) => {
   //
   next();
 };
+
+module.exports.editAccounts = (req, res, next) => {
+  // validate dữ liệu
+  if (!req.body.fullName || !req.body.email) {
+    req.flash("error", "Vui lòng điền đầy đủ thông tin tài khoản");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  //
+  next();
+};
