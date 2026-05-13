810264561680b49397128cb1aca590f52de9fd5c
Do Tien <tiendt4180@ut.edu.vn>
2026-05-13 22:21:25 +0700

    12.8_2 test login flase

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,7 +1,27 @@
 //  GET admin/auth/login
-
+const Account = require("./../../models/account.model.js");
 module.exports.login = (req, res) => {
   res.render("admin/pages/auth/login", {
     pageTitle: "Login",
   });
+};
+
+//  GET admin/auth/login
+
+module.exports.loginPost = async (req, res) => {
+  console.log(req.body);
+  // const { email, password } = req.body;
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await Account.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect(req.get("Referrer") || "/");
+
+    return;
+  }
+  res.send("");
 };
--- router/admin/auth.router.js
+++ router/admin/auth.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controler = require("../../controllers/admin/auth.controller");
 
 router.get("/login", controler.login);
+router.post("/login", controler.loginPost);
 
 module.exports = router;
