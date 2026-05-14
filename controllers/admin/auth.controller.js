c7d4768fcaa715795596f0147b12c879d2652656
Do Tien <tiendt4180@ut.edu.vn>
2026-05-14 22:50:58 +0700

    12.8_3 check pass,status auth

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,5 +1,7 @@
-//  GET admin/auth/login
 const Account = require("./../../models/account.model.js");
+const md5 = require("md5");
+//  GET admin/auth/login
+
 module.exports.login = (req, res) => {
   res.render("admin/pages/auth/login", {
     pageTitle: "Login",
@@ -17,11 +19,25 @@ module.exports.loginPost = async (req, res) => {
     email: email,
     deleted: false,
   });
+
   if (!user) {
     req.flash("error", "Email không tồn tại");
     res.redirect(req.get("Referrer") || "/");
-
     return;
   }
-  res.send("");
+
+  if (md5(password) !== user.password) {
+    req.flash("error", "Mật khẩu không đúng");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  if (user.status === "inactive") {
+    req.flash(
+      "error",
+      "Tài khoản của bạn đã bị khóa, vui lòng liên hệ quản trị viên để biết thêm chi tiết",
+    );
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  res.redirect("/admin/accounts");
 };
