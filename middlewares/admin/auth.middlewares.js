dfa1b4001736a4236180117d970bb5cdd337e11f
Do Tien <tiendt4180@ut.edu.vn>
2026-05-20 21:26:16 +0700

    13.3_1 middlewares update

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,8 +1,14 @@
-module.exports.requireAuth = (req, res, next) => {
+const Account = require("../../models/account.model");
+module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect("/admin/auth/login");
   } else {
-    console.log("token: ", req.cookies.token);
-    next();
+    const user = await Account.findOne({ token: req.cookies.token });
+    if (!user) {
+      res.redirect("/admin/auth/login");
+    } else {
+      console.log("token: ", req.cookies.token);
+      next();
+    }
   }
 };
