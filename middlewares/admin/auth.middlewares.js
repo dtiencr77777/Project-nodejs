76dcb6bd13ee64be11251076b9a3de8c5c20ea9d
Do Tien <tiendt4180@ut.edu.vn>
2026-05-20 22:16:29 +0700

    13.4 tìm role và hiển thị trên dashboard - đồng thời khi có authorization mới truy cập được

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,13 +1,22 @@
 const Account = require("../../models/account.model");
+const Role = require("../../models/roles.models");
+
 module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
     res.redirect("/admin/auth/login");
   } else {
-    const user = await Account.findOne({ token: req.cookies.token });
+    const user = await Account.findOne({ token: req.cookies.token }).select(
+      "-password -token",
+    );
     if (!user) {
       res.redirect("/admin/auth/login");
     } else {
-      console.log("token: ", req.cookies.token);
+      // console.log("token: ", req.cookies.token);
+      const role = await Role.findOne({
+        _id: user.role_id,
+      });
+      res.locals.user = user;
+      res.locals.role = role;
       next();
     }
   }
