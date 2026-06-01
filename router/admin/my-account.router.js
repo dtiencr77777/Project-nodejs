ce02ab26d65cf95825f1c38ac522e238d9e3fb21
Do Tien <tiendt4180@ut.edu.vn>
2026-06-01 23:24:14 +0700

    router edit[GET]

--- router/admin/my-account.router.js
+++ router/admin/my-account.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controler = require("../../controllers/admin/my-account.controller");
 
 router.get("/", controler.index);
+router.get("/edit", controler.edit);
 
 module.exports = router;
