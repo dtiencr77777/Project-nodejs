e54f332cc1112e3ea990f2d573dd9f7b3fc51978
Do Tien <tiendt4180@ut.edu.vn>
2026-06-03 23:08:14 +0700

    home.controller => layoutProductsCategory

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,13 @@
-module.exports.index = (req, res) => {
+const ProductCategory = require("../../models/product-category.model");
+const createTreeHelper = require("../../helpers/createTree");
+// GET /home
+module.exports.index = async (req, res) => {
+  const productsCategory = await ProductCategory.find({
+    deleted: false,
+  });
+  const newProductsCategory = createTreeHelper.tree(productsCategory);
   res.render("client/pages/home/index.pug", {
     pageTitle: "Home Page",
+    layoutProductsCategory: newProductsCategory,
   });
 };
