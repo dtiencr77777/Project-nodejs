279bc453951c0688e939553c95556e69b8b89c0a
Do Tien <tiendt4180@ut.edu.vn>
2026-06-06 22:03:22 +0700

    model featured

--- models/product.model.js
+++ models/product.model.js
@@ -15,6 +15,7 @@ const productschema = new mongoose.Schema(
     stock: Number,
     thumbnail: String,
     status: String,
+    featured: String,
     position: Number,
     slug: {
       type: String,
