2be587651ea58fca52ee188263c36a356811596d
Do Tien <tiendt4180@ut.edu.vn>
2026-06-07 19:52:51 +0700

    15.1 console.log : productsFeatured

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,14 @@
+const Product = require("../../models/product.model");
+
 // GET /home
 module.exports.index = async (req, res) => {
+  // lấy ra sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured: "1",
+  });
+  console.log(productsFeatured);
   res.render("client/pages/home/index.pug", {
     pageTitle: "Home Page",
+    productsFeatured: productsFeatured,
   });
 };
