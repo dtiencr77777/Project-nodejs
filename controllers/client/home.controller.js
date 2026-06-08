2c6ec5992e7e8377fb3be154b25dbd706f213ff8
Do Tien <tiendt4180@ut.edu.vn>
2026-06-08 23:54:33 +0700

    15.2 addd productsNew

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -10,8 +10,18 @@ module.exports.index = async (req, res) => {
   });
   const Products = priceNewHelper.priceNew(productsFeatured);
   // console.log(Products);
+  //  hiển thị danh sách sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+  //end  hiển thị danh sách sản phẩm mới nhất
+
   res.render("client/pages/home/index.pug", {
     pageTitle: "Home Page",
     productsFeatured: Products,
+    productsNew: productsNew,
   });
 };
