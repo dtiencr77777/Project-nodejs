a3f98a63900be35b2984c4ef7f0804be0386416a
Do Tien <tiendt4180@ut.edu.vn>
2026-06-09 19:50:54 +0700

    15.2_2 hiển thị =danh sách sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,7 +2,7 @@ const Product = require("../../models/product.model");
 const priceNewHelper = require("../../helpers/product");
 // GET /home
 module.exports.index = async (req, res) => {
-  // lấy ra sản phẩm nổi bật
+  //1  lấy ra sản phẩm nổi bật
   const productsFeatured = await Product.find({
     featured: "1",
     // deleted: false,
@@ -10,14 +10,14 @@ module.exports.index = async (req, res) => {
   });
   const Products = priceNewHelper.priceNew(productsFeatured);
   // console.log(Products);
-  //  hiển thị danh sách sản phẩm mới nhất
+  //2   hiển thị danh sách sản phẩm mới nhất
   const productsNew = await Product.find({
     deleted: false,
     status: "active",
   })
     .sort({ position: "desc" })
     .limit(6);
-  //end  hiển thị danh sách sản phẩm mới nhất
+  // 2end  hiển thị danh sách sản phẩm mới nhất
 
   res.render("client/pages/home/index.pug", {
     pageTitle: "Home Page",
