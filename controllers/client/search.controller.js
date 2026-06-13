8018fbd13622f4f227fda3171d904ae042b1ba78
Do Tien <tiendt4180@ut.edu.vn>
2026-06-13 23:20:06 +0700

    16.1_ complete form search

--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -1,3 +1,20 @@
-module.exports.index = (req, res) => {
-  res.send("Đây là trang tìm kiếm");
+// 1.search
+const Product = require("../../models/product.model");
+const ProductNewPriceHELPER = require("../../helpers/product");
+module.exports.index = async (req, res) => {
+  const keyword = req.query.keyword;
+  let newProduct = [];
+  if (keyword) {
+    const regex = new RegExp(keyword, "i");
+    const products = await Product.find({
+      title: regex,
+      // deleted: false,
+    });
+    newProduct = ProductNewPriceHELPER.priceNew(products);
+  }
+  res.render("client/pages/search/index", {
+    pageTitle: "Kết quả tìm kiếm",
+    keyword: keyword,
+    products: newProduct,
+  });
 };
