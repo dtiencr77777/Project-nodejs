0d2b5f3b76cee46cf6b448c799c0900e46a7802f
Do Tien <tiendt4180@ut.edu.vn>
2026-06-13 22:40:06 +0700

    controller form search basic

--- router/client/index.router.js
+++ router/client/index.router.js
@@ -1,7 +1,11 @@
 const ProuctRouter = require("./product.router");
 const HomeRouter = require("./home.router");
+const SearchRouter = require("./search.router");
+
 const categoryMiddleware = require("../../middlewares/client/caterogy.middlewares");
+
 module.exports = (app) => {
   app.use("/", categoryMiddleware.category, HomeRouter);
   app.use("/products", categoryMiddleware.category, ProuctRouter);
+  app.use("/search", SearchRouter);
 };
