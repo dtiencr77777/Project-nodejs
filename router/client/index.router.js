1ca91bad59935519e5e00a1ae47d687f44d332a2
Do Tien <tiendt4180@ut.edu.vn>
2026-06-15 21:57:18 +0700

    add cart middlewares -> router_client

--- router/client/index.router.js
+++ router/client/index.router.js
@@ -3,9 +3,11 @@ const HomeRouter = require("./home.router");
 const SearchRouter = require("./search.router");
 
 const categoryMiddleware = require("../../middlewares/client/caterogy.middlewares");
-
+const cartMiddleware = require("../../middlewares/client/cart.middlewares");
 module.exports = (app) => {
-  app.use("/", categoryMiddleware.category, HomeRouter);
-  app.use("/products", categoryMiddleware.category, ProuctRouter);
+  app.use(categoryMiddleware.category);
+  app.use(cartMiddleware.cartId);
+  app.use("/", HomeRouter);
+  app.use("/products", ProuctRouter);
   app.use("/search", SearchRouter);
 };
