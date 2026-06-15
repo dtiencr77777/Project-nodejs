2107212bfd97441c5cee1ca1941aeab929099ec7
Do Tien <tiendt4180@ut.edu.vn>
2026-06-15 22:00:46 +0700

    set time cho cookie

--- middlewares/client/cart.middlewares.js
+++ middlewares/client/cart.middlewares.js
@@ -5,7 +5,10 @@ module.exports.cartId = async (req, res, next) => {
     // chưa có thì phải tạo
     const cart = new Cart();
     await cart.save();
-    res.cookie("cartId", cart.id);
+    const expriresCookie = 1000 * 30 * 60;
+    res.cookie("cartId", cart.id, {
+      expires: new Date(Date.now() + expriresCookie),
+    });
   } else {
     // có rồi chỉ cần lấy ra thôi
   }
