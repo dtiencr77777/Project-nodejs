95f4e942496e11173e61d3ee13777a65e8fe033e
Do Tien <tiendt4180@ut.edu.vn>
2026-06-16 15:22:02 +0700

    16.3_ thêm sản phẩm vào giỏ hàng. => carts/add/:productsId

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,6 +1,25 @@
+const Cart = require("../../models/cart.model");
+
+// [POST]  /cart/add/:productId
 module.exports.addPost = async (req, res) => {
   const productId = req.params.productId;
   const quantity = req.body.quantity;
-  console.log(productId, quantity);
-  res.send("ok");
+  const cartId = req.cookies.cartId;
+  console.log(cartId);
+  console.log(productId);
+  console.log(quantity);
+  const objectCart = {
+    product_id: productId,
+    quantity: quantity,
+  };
+  await Cart.updateOne(
+    {
+      _id: cartId,
+    },
+    {
+      $push: { products: objectCart },
+    },
+  );
+  req.flash("success", "Đã thêm vào giỏ hàng");
+  res.redirect(req.get("Referrer") || "/");
 };
