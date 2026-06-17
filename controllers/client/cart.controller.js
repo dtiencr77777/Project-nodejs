9aa5ace198bc816dec0e97ccffe44e6d5bd3bfcf
Do Tien <tiendt4180@ut.edu.vn>
2026-06-17 22:01:45 +0700

    lặp qua từng phần tử để truyền data sang pug fe

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,9 +1,26 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
 
 //  GET /cart/
 module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
+      const productId = item.product_id;
+      const productInfo = await Product.findOne({
+        _id: productId,
+      }).select("title thumbnail slug price discountPercentage");
+      item.productInfo = productInfo;
+    }
+  }
+  console.log(cart);
+
   res.render("client/pages/cart/index", {
     pageTitle: "Giỏ hàng",
+    cartDetail: cart,
   });
 };
 
