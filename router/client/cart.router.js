b0e00817ec800c46535dc8c8b9593f9b027f1ae0
Do Tien <tiendt4180@ut.edu.vn>
2026-06-17 23:40:45 +0700

    17.2_add router cart/delete/:productId

--- router/client/cart.router.js
+++ router/client/cart.router.js
@@ -5,5 +5,5 @@ const controler = require("../../controllers/client/cart.controller");
 
 router.get("/", controler.index);
 router.post("/add/:productId", controler.addPost);
-
+router.get("/delete/:productId", controler.delete);
 module.exports = router;
