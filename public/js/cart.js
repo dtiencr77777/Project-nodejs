89975f315a15691a8c1d142ee0c7272964fd1d54
Do Tien <tiendt4180@ut.edu.vn>
2026-06-19 22:55:10 +0700

    script /cart quantity done

--- public/js/cart.js
+++ public/js/cart.js
@@ -5,7 +5,10 @@ const inputsQuantity = document.querySelectorAll("input[name='quantity']");
 if (inputsQuantity.length > 0) {
   inputsQuantity.forEach((input) => {
     input.addEventListener("change", (e) => {
-      console.log(e.target.value);
+      // console.log(e.target.value);
+      const productId = input.getAttribute("product-id");
+      const quantity = input.value;
+      window.location.href = `/cart/update/${productId}/${quantity}`;
     });
   });
 }
