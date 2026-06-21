8aa55f119a12dce4b4eca0288f69c9a9b3dde708
Do Tien <tiendt4180@ut.edu.vn>
2026-06-21 20:50:34 +0700

    truy vấn dữ liệu từ cart sang checkout

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,6 +1,29 @@
 // get : checkout
 module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
+      const productId = item.product_id;
+      const productInfo = await Product.findOne({
+        _id: productId,
+      }).select("title thumbnail slug price discountPercentage");
+
+      productInfo.priceNew = productHelper.priceNewProduct(productInfo);
+
+      item.productInfo = productInfo;
+      item.totalPrice = item.productInfo.priceNew * item.quantity;
+    }
+  }
+  // console.log(cart);
+  cart.totalPrice = cart.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0,
+  );
   res.render("client/pages/checkout/index", {
-    pageTitle: "Đặt hàng",
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart,
   });
 };
