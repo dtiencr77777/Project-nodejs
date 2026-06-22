cf071618775bf8e4d836ea248b5e8a503efd4fe0
Do Tien <tiendt4180@ut.edu.vn>
2026-06-22 21:12:12 +0700

    17.4_2 router /checkout/success đặt hàng thành công

--- router/client/checkout.router.js
+++ router/client/checkout.router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controler = require("../../controllers/client/checkout.controller");
 router.get("/", controler.index);
 router.post("/order", controler.order);
+router.get("/success/:orderId", controler.success);
 
 module.exports = router;
