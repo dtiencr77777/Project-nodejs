103b2e34051fd294b0366e1eb6356743fa4ee53b
Do Tien <tiendt4180@ut.edu.vn>
2026-06-23 16:21:07 +0700

    18.4 user/login and validate email/passwork

--- router/client/user.router.js
+++ router/client/user.router.js
@@ -5,5 +5,7 @@ const controler = require("../../controllers/client/user.controller");
 const validate = require("../../validates/client/user.validate");
 router.get("/register", controler.register);
 router.post("/register", validate.regiterPost, controler.registerPost);
+router.get("/login", controler.login);
+router.post("/login", validate.loginPost, controler.loginPost);
 
 module.exports = router;
--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -16,3 +16,17 @@ module.exports.regiterPost = (req, res, next) => {
   }
   next();
 };
+
+module.exports.loginPost = (req, res, next) => {
+  if (!req.body.email) {
+    res.flash("error", "Vui lòng điền đầy đủ thông tin tài khoản");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  if (!req.body.password) {
+    res.flash("error", "Vui lòng điền đầy đủ thông tin tài khoản");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  next();
+};
