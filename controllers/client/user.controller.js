bdb51e2a4d9eb1ea1efaa98815833f6d389c7dac
Do Tien <tiendt4180@ut.edu.vn>
2026-06-23 15:43:58 +0700

    18.3 add router /registerPost

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,6 +1,29 @@
+const md5 = require("md5");
+const User = require("../../models/user.model");
 // GET : user/regiter
 module.exports.register = async (req, res) => {
   res.render("client/pages/user/register", {
     pageTitle: "Đăng ký",
   });
+};
+
+// POST : user/regiter
+module.exports.registerPost = async (req, res) => {
+  console.log(req.body);
+  const exitsEmail = await User.findOne({
+    email: req.body.email,
+  });
+  if (exitsEmail) {
+    req.flash("error", "Email đã tồn tại");
+    res.redirect("/user/register");
+    return;
+  }
+  req.body.password = md5(req.body.password);
+  const user = new User(req.body);
+  await user.save();
+  req.flash("success", "Đăng ký thành công");
+  // console.log(user);
+  // lưu tokenUser vào cookie
+  res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/");
 };
--- router/client/user.router.js
+++ router/client/user.router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const controler = require("../../controllers/client/user.controller");
+const validate = require("../../validates/client/user.validate");
 router.get("/register", controler.register);
+router.post("/register", validate.regiterPost, controler.registerPost);
 
 module.exports = router;
