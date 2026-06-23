86a1e4ad9312587d5e296116fedd41b2e5e9b820
Do Tien <tiendt4180@ut.edu.vn>
2026-06-23 16:40:28 +0700

    18.5 middlewares hiển thị login/logout

--- router/client/index.router.js
+++ router/client/index.router.js
@@ -7,9 +7,14 @@ const UserRouter = require("./user.router");
 
 const categoryMiddleware = require("../../middlewares/client/caterogy.middlewares");
 const cartMiddleware = require("../../middlewares/client/cart.middlewares");
+// middlewares dành cho không hiển thị thanh header
+const userMiddleware = require("../..//middlewares/client/user.middlewares");
 module.exports = (app) => {
+  // middlewares
   app.use(categoryMiddleware.category);
   app.use(cartMiddleware.cartId);
+  app.use(userMiddleware.infoUser);
+  // router
   app.use("/", HomeRouter);
   app.use("/products", ProuctRouter);
   app.use("/search", SearchRouter);
