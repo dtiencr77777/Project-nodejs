4bb64c973bde93aed819e9c990b60a2e37eff672
Do Tien <tiendt4180@ut.edu.vn>
2026-06-23 16:21:33 +0700

    controller user/login -- loginPost

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -27,3 +27,39 @@ module.exports.registerPost = async (req, res) => {
   res.cookie("tokenUser", user.tokenUser);
   res.redirect("/");
 };
+
+// GET : user/login
+module.exports.login = async (req, res) => {
+  res.render("client/pages/user/login", {
+    pageTitle: "Đăng nhập",
+  });
+};
+
+// POST user/login
+module.exports.loginPost = async (req, res) => {
+  // console.log(req.body);
+  const email = req.body.email;
+  const password = md5(req.body.password);
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect("/user/login");
+    return;
+  }
+  if (password !== user.password) {
+    req.flash("error", "Mật khẩu không đúng");
+    res.redirect("/user/login");
+    return;
+  }
+  if (user.status == "inactive") {
+    req.flash("error", "Tài khoản đã bị khóa");
+    res.redirect("/user/login");
+    return;
+  }
+  res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/");
+};
