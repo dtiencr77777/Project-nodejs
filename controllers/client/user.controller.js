17fc4c789fbd5b6992ae204e88263d63d6cf2c5f
Do Tien <tiendt4180@ut.edu.vn>
2026-06-30 20:39:36 +0700

    19.10 khi login thì cập nhật cartId , logout thì clear cookie cartId

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -68,16 +68,23 @@ module.exports.loginPost = async (req, res) => {
   }
 
   //  thêm user vào Model : Cart
-  // console.log(req.cookies.cartId);
-  // console.log(user.id);
-  await Cart.updateOne(
-    {
-      _id: req.cookies.cartId,
-    },
-    {
-      user_id: user.id,
-    },
-  );
+  const cart = await Cart.findOne({
+    user_id: user.id,
+  });
+
+  if (cart) {
+    res.cookie("cartId", cart.id);
+  } else {
+    await Cart.updateOne(
+      {
+        _id: req.cookies.cartId,
+      },
+      {
+        user_id: user.id,
+      },
+    );
+  }
+
   res.cookie("tokenUser", user.tokenUser);
   res.redirect("/");
 };
@@ -85,6 +92,7 @@ module.exports.loginPost = async (req, res) => {
 // GET : user/logout
 module.exports.logout = async (req, res) => {
   res.clearCookie("tokenUser");
+  res.clearCookie("cartId");
   res.redirect("/");
 };
 
