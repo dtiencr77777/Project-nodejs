6cb87ac8e00f84d87e97b116e101e4999488c970
Do Tien <tiendt4180@ut.edu.vn>
2026-06-30 22:44:29 +0700

    20.2 upload settings/general nhưng lỗi req.body không load dữ liệu

--- controllers/admin/setting.controller.js
+++ controllers/admin/setting.controller.js
@@ -1,6 +1,16 @@
+const SettingGeneral = require("../../models/settings-general.model");
 // GET admin/settings/general
-module.exports.general = (req, res) => {
+module.exports.general = async (req, res) => {
+  const settingGeneral = await SettingGeneral.findOne({});
   res.render("admin/pages/settings/general.pug", {
     pageTitle: "Cài đặt chung",
+    settingGeneral: settingGeneral,
   });
+};
+
+//PATCH admin/settings/general
+module.exports.generalPatch = (req, res) => {
+  const settingGeneral = new SettingGeneral(req.body);
+  settingGeneral.save();
+  res.redirect("/admin/settings/general");
 };
--- router/admin/settings-general.router.js
+++ router/admin/settings-general.router.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+//  Hiển thị ảnh sản phẩm
+const multer = require("multer");
+// const upload = multer({ dest: "./public/uploads/" });
 
+//  cloudinary
+// const storageMulter = require("../../helpers/storageMulter");
+// const upload = multer({ storage: storageMulter() });
+const upload = multer();
+const uploadCloudRouter = require("../../middlewares/admin/uploadCloud.middlewares");
+//end  cloudinary
 const controler = require("../../controllers/admin/setting.controller");
 
 router.get("/general", controler.general);
+router.patch("/general", controler.generalPatch);
 
 module.exports = router;
