fffa7fc191b2c12a04b532bf7732adf9539ccc46
Do Tien <tiendt4180@ut.edu.vn>
2026-07-01 16:34:30 +0700

    data giả truyền qua pug

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,6 +1,29 @@
 //  GET /admin/dashboard
 module.exports.dashboard = (req, res) => {
+  const statistic = {
+    categoryProduct: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    product: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  };
   res.render("admin/pages/dashboard/index.pug", {
     pageTitle: "Dashboard",
+    statistic: statistic,
   });
 };
