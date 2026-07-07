678e237219cc48038e0501368b878feb88a97656
Do Tien <tiendt4180@ut.edu.vn>
2026-07-07 21:37:12 +0700

    gửi data từ FE => BE và lưu vào model database

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,8 +1,21 @@
+const Chat = require("../../models/chat.model");
 // GET : /chat
 module.exports.index = async (req, res) => {
+  const userId = req.locals.user.id;
+
+  // socket.io
   _io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+    // console.log("a user connected", socket.id);
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      // lưu vào database
+      const chat = new Chat({
+        user_id: userId,
+        content: content,
+      });
+      await chat.save();
+    });
   });
+
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
   });
