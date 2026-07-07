37879342ddcf76362bb5725685d115ee0a5a484f
Do Tien <tiendt4180@ut.edu.vn>
2026-07-07 21:36:36 +0700

    21.5 frontend js tìm formsendDât

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,15 @@
-console.log("ok");
+//  CLIENT SEND MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+// console.log(formSendData);
+
+if (formSendData) {
+  formSendData.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const content = e.target.elements.content.value;
+    console.log(content);
+    if (content) {
+      socket.emit("CLIENT_SEND_MESSAGE", { content });
+      e.target.elements.content.value = "";
+    }
+  });
+}
