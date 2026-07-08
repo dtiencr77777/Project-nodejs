bfa5883b1a1ae0fa3ec4a0bebdc093476c68f742
Do Tien <tiendt4180@ut.edu.vn>
2026-07-08 22:50:14 +0700

    add infoUser vào Chatmodel

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,4 +1,5 @@
 const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 // GET : /chat
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
@@ -17,7 +18,17 @@ module.exports.index = async (req, res) => {
     });
   });
 
+  const chats = await Chat.find({
+    deleted: false,
+  });
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id,
+    }).select("fullName");
+    chat.infoUser = infoUser;
+  }
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
+    chats: chats,
   });
 };
