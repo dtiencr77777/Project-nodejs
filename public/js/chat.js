cb9f71c9c832da32d13661f9244135148403236a
Do Tien <tiendt4180@ut.edu.vn>
2026-07-09 22:02:55 +0700

    21.8 update tính năng realtime

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,3 +13,21 @@ if (formSendData) {
     }
   });
 }
+
+// end CLIENT SEND MESSAGE
+
+// server return message
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  // console.log(data);
+  const body = document.querySelector(".chat .inner-body");
+  const div = document.createElement("div");
+  div.classList.add("inner-incoming");
+  div.innerHTML = `
+      <div class="inner-name">${data.fullName}</div>
+      <div class="inner-content">${data.content}</div>
+
+  `;
+  body.appendChild(div);
+});
+
+// end  server return message
